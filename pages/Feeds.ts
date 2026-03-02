8a952e152508398691efbb96363198fa387fa9e7
sarathrajautomation <sarathrajk@techcedence.com>
2026-03-02 18:46:42 +0530

    Added pulse with media upload

--- pages/Feeds.ts
+++ pages/Feeds.ts
@@ -1,4 +1,6 @@
 import { Page, expect } from "@playwright/test";
+import { log } from "console";
+import path from "path/win32";
 
 export class Feeds {
   constructor(private page: Page) {}
@@ -61,12 +63,41 @@ export class Feeds {
     await this.page.goto("https://unitedtribes.techcedence.net/pulse", {
       waitUntil: "domcontentloaded",
     });
+    const filePath = path.join(__dirname, "..", "insurance~.png");
     await this.page.getByText("What's on your mind?").click();
+    await this.page.locator("(//b[text()=' Upload Media '])[2]").click();
+    await this.page.setInputFiles('input[type="file"]', filePath);
 
+    await this.page.locator("//button[text()=' Post ']").click();
+    await this.deleteAllPosts();
     // Implement like and comment steps
   }
 
-  async checkFeedDeletion() {
-    // Implement deletion steps
+  async deleteAllPosts() {
+    await this.page.goto(
+      "https://unitedtribes.techcedence.net/profile/my-pulse",
+      { waitUntil: "domcontentloaded" },
+    );
+
+    const posts = this.page.locator("app-feed-card");
+    console.log(await posts.count());
+
+    while ((await posts.count()) > 0) {
+      const firstPost = posts.first();
+
+      // Click 3-dot inside this post only
+      await firstPost.locator(".fa-ellipsis-vertical").click();
+
+      // Click Delete option from dropdown
+      await this.page.locator("text=Delete").click();
+
+      // Confirm delete
+      await this.page.locator("button:has-text('Delete')").click();
+
+      // Wait until this post disappears from DOM
+      await firstPost.waitFor({ state: "detached" });
+    }
+
+    console.log("All posts deleted successfully");
   }
 }
